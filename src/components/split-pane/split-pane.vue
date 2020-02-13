2f54fe34dba4b71b618cf9212106c7e1f749fe57
darenone <1291342008@qq.com>
2020-02-13 11:59:02 +0800

    浅谈vue中如何操作DOM-2

--- src/components/split-pane/split-pane.vue
+++ src/components/split-pane/split-pane.vue
@@ -1,8 +1,11 @@
 <template>
-    <div class="split-pane-wrapper">
-        <div class="pane pane-left" :style="{ width: leftOffsetPercent }"></div>
-        <div class="pane-trigger-con" :style="{ left: triggerLeft, width: `${triggerWidth}px` }"></div>
+    <div class="split-pane-wrapper" ref="outer">
+        <div class="pane pane-left" :style="{ width: leftOffsetPercent }">
+            <slot name="left"></slot>
+        </div>
+        <div class="pane-trigger-con" :style="{ left: triggerLeft, width: `${triggerWidth}px` }" @mousedown="handleMouseDown"></div>
         <div class="pane pane-right" :style="{ left: leftOffsetPercent }">
+            <slot name="right"></slot>
             <button @click="handleClick">改变宽度</button>
         </div>
     </div>
@@ -14,11 +17,25 @@ export default {
         triggerWidth: {
             type: Number,
             default: 8
+        },
+        min: {
+            type: Number,
+            default: 0.1
+        },
+        max: {
+            type: Number,
+            default: 0.9
+        },
+        leftOffset: {
+            type: Number,
+            default: 0.5
         }
     },
     data() {
         return {
-            leftOffset: 0.3
+            canMove: false, // 鼠标弹起
+            initOffset: 0, // 初始偏移量
+
         }
     },
     computed: {
@@ -32,6 +49,30 @@ export default {
     methods: {
         handleClick () {
             this.leftOffset -= 0.02
+        },
+        handleMouseDown (event) {
+            document.addEventListener('mousemove', this.handleMouseMove)
+            document.addEventListener('mouseup', this.handleMouseUp)
+            // event.srcElement.getBoundingClientRect().left 小红条距离左侧的距离
+            this.initOffset = event.pageX - event.srcElement.getBoundingClientRect().left
+            this.canMove = true
+        },
+        handleMouseUp () { // 记录鼠标弹起事件
+            this.canMove = false
+        },
+        handleMouseMove (event) {
+            /** 
+             * event.pageX 鼠标箭头距离页面左侧的距离（单位像素）
+             * this.$refs.outer 获取到DOM对象
+             * const object = this.$refs.outer.getBoundingClientRect() 调用DOM对象的方法，此方法返回一个对象,IE9也支持此方法
+            */
+           if (!this.canMove) return;
+           const object = this.$refs.outer.getBoundingClientRect()
+           let offsetPercent = (event.pageX - object.left - this.initOffset + this.triggerWidth / 2) / object.width
+           if (offsetPercent < this.min) offsetPercent = this.min
+           if (offsetPercent > this.max) offsetPercent = this.max
+            // this.leftOffset = offsetPercent // 转化为% 子组件不能修改传入的值，通过事件让父组件修改
+            this.$emit('update', offsetPercent)
         }
     }
 }
@@ -61,6 +102,8 @@ export default {
             position: absolute;
             z-index: 10;
             top: 0;
+            user-select: none; // 取消选中效果
+            cursor: col-resize;
         }
     }
 }
--- src/views/split-pane.vue
+++ src/views/split-pane.vue
@@ -1,6 +1,9 @@
 <template>
     <div class="split-pane-con">
-        <split-pane></split-pane>
+        <split-pane :leftOffset="leftOffset" @update="handleUpdate">
+            <div slot="left">左侧内容</div>
+            <div slot="right">右侧内容</div>
+        </split-pane>
     </div>
 </template>
 <script>
@@ -8,6 +11,16 @@ import SplitPane from '_c/split-pane'
 export default {
     components: {
         SplitPane
+    },
+    data () {
+        return {
+            leftOffset: 0.5
+        }
+    },
+    methods: {
+        handleUpdate (val) {
+            this.leftOffset = val
+        }
     }
 }
 </script>
